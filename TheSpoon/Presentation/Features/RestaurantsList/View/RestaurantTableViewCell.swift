acefdfd9791116b6f289dda8363ccd05dfdfd4bb
Gabriele Pavanello <gabriele.pavanello@accenture.com>
2021-12-30 09:10:11 +0100

    Refactor table view cell

--- TheSpoon/Presentation/Features/RestaurantsList/View/RestaurantTableViewCell.swift
+++ TheSpoon/Presentation/Features/RestaurantsList/View/RestaurantTableViewCell.swift
@@ -11,6 +11,7 @@ import Swinject
 
 class RestaurantTableViewCell: UITableViewCell, ItemViewModelBindable {
     
+    //MARK: Constants
     private struct ViewConst {
         static let imageHeight: CGFloat = 210
         static let ratingWidth: CGFloat = 80
@@ -20,44 +21,79 @@ class RestaurantTableViewCell: UITableViewCell, ItemViewModelBindable {
         static let avgPriceText: String = "Average price"
     }
     
+    //MARK: Properties
     private var viewModel: RestaurantItemViewModel? = nil
     private var disposable: Disposable? = nil
     
-    @UsesAutoLayout
-    var name: UILabel = UILabel()
+    //MARK: Subviews and Constraints
+    ///Name of the restaurant
+    lazy var name: UILabel = {
+        @UsesAutoLayout
+        var label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 23, weight: .bold)
+        label.textColor = UIColor.titleColor
+        label.lineBreakMode = .byTruncatingTail
+        label.numberOfLines = 2
+        return label
+    }()
     
-    @UsesAutoLayout
-    var address: UILabel = UILabel()
+    ///Address of the restaurant
+    lazy var address: UILabel = {
+        @UsesAutoLayout
+        var label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .light)
+        label.textColor = UIColor.subtitleColor
+        label.lineBreakMode = .byTruncatingTail
+        label.numberOfLines = 2
+        return label
+    }()
     
+    ///Pricing info of the restaurant
     @UsesAutoLayout
     var pricingView: PricingView = PricingView(frame: .zero)
     
+    ///Rating info of the restaurant
     @UsesAutoLayout
     var rating: RatingView = RatingView(frame: .zero)
     
-    @UsesAutoLayout
-    var imgView: UIImageView = UIImageView()
+    ///Image of the restaurant
+    lazy var imgView: UIImageView = {
+        @UsesAutoLayout
+        var image = UIImageView()
+        image.layer.cornerRadius = 5.0
+        image.clipsToBounds = true
+        image.backgroundColor = .lightGray
+        image.contentMode = .scaleAspectFill
+        return image
+    }()
     private lazy var imgViewConstraints: [NSLayoutConstraint] = {
         [
             imgView.topAnchor
                 .constraint(equalTo: contentView.topAnchor,
-                        constant: Layout.topPadding),
-        
+                            constant: Layout.topPadding),
+            
             imgView.leadingAnchor
                 .constraint(equalTo: contentView.leadingAnchor,
-                        constant: Layout.leftPadding),
-        
+                            constant: Layout.leftPadding),
+            
             imgView.trailingAnchor
                 .constraint(equalTo: contentView.trailingAnchor,
-                        constant: Layout.rightPadding),
-        
+                            constant: Layout.rightPadding),
+            
             imgView.heightAnchor
                 .constraint(equalToConstant: ViewConst.imageHeight)
         ]
     }()
     
-    @UsesAutoLayout
-    var cuisine: UILabel = UILabel()
+    ///Cuisine type of the restaurant
+    lazy var cuisine: UILabel = {
+        @UsesAutoLayout
+        var label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        label.textColor = UIColor.subtitleColor
+        label.layer.opacity = 0.4
+        return label
+    }()
     private lazy var cuisineConstraints: [NSLayoutConstraint] = {
         [
             cuisine.leadingAnchor
@@ -69,8 +105,32 @@ class RestaurantTableViewCell: UITableViewCell, ItemViewModelBindable {
         ]
     }()
     
-    @UsesAutoLayout
-    var favouriteBtn = UIButton(type: .custom)
+    ///Toggle favourite button of the restaurant.
+    lazy var favouriteBtn: UIButton = {
+        @UsesAutoLayout
+        var button = UIButton(type: .custom)
+        button.contentMode = .scaleAspectFit
+        button.contentHorizontalAlignment = .fill
+        button.contentVerticalAlignment = .fill
+        
+        if let image = UIImage(named: ViewConst.disabledFavBtnName)?
+            .withRenderingMode(.alwaysTemplate) {
+            button.setImage(image, for: .normal)
+        }
+        
+        if let image = UIImage(named: ViewConst.enabledFavBtnName)?
+            .withRenderingMode(.alwaysTemplate){
+            button.setImage(image, for: .selected)
+        }
+        button.imageView?.tintColor = .white
+        button.backgroundColor = .lightGray
+        button.imageEdgeInsets = UIEdgeInsets(top: 8,left: 6,bottom: 7,right: 6)
+        button.addTarget(self,
+                         action: #selector(self.favouriteBtnTapped),
+                         for: .touchUpInside)
+        button.layer.cornerRadius = ViewConst.squaredBtnFavSize / 2
+        return button
+    }()
     private lazy var favouriteBtnConstraints: [NSLayoutConstraint] = {
         [
             favouriteBtn.topAnchor
@@ -89,6 +149,7 @@ class RestaurantTableViewCell: UITableViewCell, ItemViewModelBindable {
         ]
     }()
     
+    ///VStack of the Restaurant Info
     lazy var stackInfo: UIStackView = {
         let stack = UIStackView(arrangedSubviews: [self.name, self.address, self.pricingView])
         stack.axis = .vertical
@@ -98,6 +159,7 @@ class RestaurantTableViewCell: UITableViewCell, ItemViewModelBindable {
         return stack
     }()
     
+    ///HStack of the Restaurant Info and Rating
     lazy var stackInfoAndRating: UIStackView = {
         let stack = UIStackView(arrangedSubviews: [self.stackInfo, self.rating])
         stack.axis = .horizontal
@@ -126,6 +188,9 @@ class RestaurantTableViewCell: UITableViewCell, ItemViewModelBindable {
         ]
     }()
     
+    
+    
+    //MARK: Init
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupSubviews()
@@ -136,55 +201,20 @@ class RestaurantTableViewCell: UITableViewCell, ItemViewModelBindable {
         setupSubviews()
     }
     
+    
+    
+    //MARK: Setup
     private func setupSubviews() {
         //Main Image
-        imgView.layer.cornerRadius = 5.0
-        imgView.clipsToBounds = true
-        imgView.backgroundColor = .lightGray
-        imgView.contentMode = .scaleAspectFill
         contentView.addSubview(imgView)
         
         //Cuisine Type
-        cuisine.font = UIFont.systemFont(ofSize: 13, weight: .regular)
-        cuisine.textColor = UIColor.subtitleColor
-        cuisine.layer.opacity = 0.4
         contentView.addSubview(cuisine)
         
-        //Restaurant Name
-        name.font = UIFont.systemFont(ofSize: 23, weight: .bold)
-        name.textColor = UIColor.titleColor
-        name.lineBreakMode = .byTruncatingTail
-        name.numberOfLines = 2
-        
-        //Address
-        address.font = UIFont.systemFont(ofSize: 16, weight: .light)
-        address.textColor = UIColor.subtitleColor
-        address.lineBreakMode = .byTruncatingTail
-        address.numberOfLines = 2
-        
         //Favourite
-        favouriteBtn.contentMode = .scaleAspectFit
-        favouriteBtn.contentHorizontalAlignment = .fill
-        favouriteBtn.contentVerticalAlignment = .fill
-        
-        if let image = UIImage(named: ViewConst.disabledFavBtnName)?
-            .withRenderingMode(.alwaysTemplate) {
-            favouriteBtn.setImage(image, for: .normal)
-        }
-        
-        if let image = UIImage(named: ViewConst.enabledFavBtnName)?
-            .withRenderingMode(.alwaysTemplate){
-            favouriteBtn.setImage(image, for: .selected)
-        }
-        favouriteBtn.imageView?.tintColor = .white
-        favouriteBtn.backgroundColor = .lightGray
-        favouriteBtn.imageEdgeInsets = UIEdgeInsets(top: 8,left: 6,bottom: 7,right: 6)
-        favouriteBtn.addTarget(self,
-                               action: #selector(self.favouriteBtnTapped),
-                               for: .touchUpInside)
-        favouriteBtn.layer.cornerRadius = ViewConst.squaredBtnFavSize / 2
-        
         contentView.addSubview(favouriteBtn)
+        
+        //Content Stack
         contentView.addSubview(stackInfoAndRating)
     }
     
@@ -203,7 +233,7 @@ class RestaurantTableViewCell: UITableViewCell, ItemViewModelBindable {
         if let img = viewModel?.data.img {
             imgView.load(url: img)
         }
-                
+        
         cuisine.text = viewModel?.data.cuisine.uppercased() ?? "-"
         name.text = viewModel?.data.name ?? "-"
         address.text = viewModel?.data.address ?? "-"
@@ -236,10 +266,16 @@ class RestaurantTableViewCell: UITableViewCell, ItemViewModelBindable {
         setupDataBinding()
     }
     
+    
+    
+    //MARK: Actions
     @objc func favouriteBtnTapped(sender: UIButton) {
         viewModel?.toggleFavourite()
     }
     
+    
+    
+    //MARK: Overrides
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(false, animated: animated)
     }
